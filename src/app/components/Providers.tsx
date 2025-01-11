37f8682b257c2f36dd554d3a6a871f7ba31caa39
Abel Lee <binghengabellee@gmail.com>
2025-01-11 15:18:48 +0800

    set up nextjs application

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,12 @@
+import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
+
+export default function Providers({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+        <NextUIProvider>{children}</NextUIProvider>
+    );
+}
---
+++ src/app/components/navbar/NavLink.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { NavbarItem } from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+type Props = {
+  href: string;
+  label: string;
+};
+
+export default function NavLink({
+  href,
+  label,
+}: Props) {
+
+  return (
+    <NavbarItem
+      as={Link}
+      href={href}
+    >
+      <span>{label}</span>
+    </NavbarItem>
+  );
+}
